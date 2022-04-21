0c00dc5f5650cf65c658c4965193e7c78eff3fe8
Nikola Marin <nightshpee20@gmail.com>
2022-04-21 23:12:22 +0300

    Added triggers that prevenet deletion of data from all the parent tables if there are any references pointing to them in the user_ tables, Added triggers that automatically delete data that has no references pointing at it in the user_ tables.

--- RM_DummyData.sql
+++ RM_DummyData.sql
@@ -1,6 +1,7 @@
 USE rmdb; 
 
 -- GENERATE USERS 
+-- (username, password, email, mobile_number)
 CALL sp_insertUser('nikolamarin','nikolamarin1', 'nikola@fake.com', '1234567890');
 CALL sp_insertUser('stefanstefanov','stefastefanov1', 'stefan@fake.com', '1234567891');
 CALL sp_insertUser('angelangelov','angelangelov1', 'angel@fake.com', '1234567892');
@@ -13,7 +14,9 @@ CALL sp_insertUser('kirilkirilov','kirilkirilov1', 'kiril@fake.com', '1234567898
 CALL sp_insertUser('ivailoivailov','ivailoivailov1', 'ivailo@fake.com', '1234567899');   
  
  
+# ALL sp_insertUser... GENERATE DATA FOR THEIR RESPECTIVE PARENT TABLES.
 -- GENERATE CITIES
+-- (city, user_id)
 CALL sp_insertUserCity('Varna', 1);
 CALL sp_insertUserCity('Burgas', 1);
 CALL sp_insertUserCity('Sofia', 1);
@@ -25,8 +28,34 @@ CALL sp_insertUserCity('Lom', 1);
 CALL sp_insertUserCity('Pernik', 1);
 CALL sp_insertUserCity('Montana', 1);
 
+CALL sp_insertUserCity('Varna', 2);
+CALL sp_insertUserCity('Burgas', 2);
+CALL sp_insertUserCity('Sofia', 2);
+CALL sp_insertUserCity('Plovdiv', 2);
+CALL sp_insertUserCity('Veliko Tarnovo', 2);
+CALL sp_insertUserCity('Stara Zagora', 2);
+CALL sp_insertUserCity('Shumen', 2);
+CALL sp_insertUserCity('Lom', 2);
+CALL sp_insertUserCity('Pernik', 2);
+CALL sp_insertUserCity('Montana', 2);
 
--- GENERATE USER_OCCUPATIONS (The procedures generate data for 'Occupations' as well.)
+
+-- GENERATE LOCATIONS
+-- (location, city_id, user_id)
+CALL sp_insertUserLocation('The dog park', 2, 1);
+CALL sp_insertUserLocation('The Cinema', 3, 1);
+CALL sp_insertUserLocation('Ted\'s Cafe', 2, 1);
+CALL sp_insertUserLocation('Secrets', 6, 1);
+CALL sp_insertUserLocation('The gym', 10, 1);
+CALL sp_insertUserLocation('The car fair', 4, 1);
+CALL sp_insertUserLocation('The library', 1, 1);
+CALL sp_insertUserLocation('Comic con', 8, 1);
+CALL sp_insertUserLocation('The beach', 2, 1);
+CALL sp_insertUserLocation('Sunday marathon', 6, 1);
+
+
+-- GENERATE USER_OCCUPATIONS
+-- (occupation, user_id)
 CALL sp_insertUserOccupation('Unemployed', 1);
 CALL sp_insertUserOccupation('Attourney', 1);
 CALL sp_insertUserOccupation('Cheff', 1);
@@ -39,7 +68,8 @@ CALL sp_insertUserOccupation('Yoga Instructor', 1);
 CALL sp_insertUserOccupation('Bartender', 1);
 
 
--- GENERATE USER_REASONS (The procedures generate data for 'Reasons' as well.)
+-- GENERATE USER_REASONS
+-- (reason, user_id)
 CALL sp_insertUserReason('Working out', 1);
 CALL sp_insertUserReason('Business meeting', 1);
 CALL sp_insertUserReason('Study Session', 1);
@@ -52,7 +82,8 @@ CALL sp_insertUserReason('Date', 1);
 CALL sp_insertUserReason('Seeing a movie', 1);
 
 
--- GENERATE USER_RELATIONSHIPS (The procedures generate data for 'Relatiships' as well.)
+-- GENERATE USER_RELATIONSHIPS
+-- (relationship, user_id)
 CALL sp_insertUserRelationship('Close Friend', 1);
 CALL sp_insertUserRelationship('Colleague', 1);
 CALL sp_insertUserRelationship('Acquaintance', 1);
@@ -64,10 +95,9 @@ CALL sp_insertUserRelationship('Student', 1);
 CALL sp_insertUserRelationship('Family', 1);
 CALL sp_insertUserRelationship('Friend', 1);
 
-CALL sp_insertUserRelationship('Family', 2);
-CALL sp_insertUserRelationship('Friend', 2);
 
--- GENERATE USER_ACQUAINTANCE_RELATIONSHIPS (The procedures generate data for 'Acquaintances' as well.)
+-- GENERATE USER_ACQUAINTANCE_RELATIONSHIPS 
+-- (user_id, first_name, last_name, gender, occupation_id, city_id, address, relationship_id)
 CALL sp_insertUserAcquaintance(1, 'Georgi', 'Georgiev', 'M', 1, 4, null, 1);
 CALL sp_insertUserAcquaintance(1, 'Nikola', 'Nikolov', 'M', 2, 1, null, 2);
 CALL sp_insertUserAcquaintance(1,'Vanesa', 'Vanesova', 'F', 1, 6, null, 2);
@@ -79,17 +109,16 @@ CALL sp_insertUserAcquaintance(1, 'Yoana', 'Yoanova', 'F', 2, 1, null, 10);
 CALL sp_insertUserAcquaintance(1, 'Angel', 'Angelov', 'M', 6, 1, null, 9);
 CALL sp_insertUserAcquaintance(1, 'Martin', 'Martinov', 'M', 1,  8, null, 4);
 
-CALL sp_insertUserAcquaintance(2, 'Yoana', 'Yoanova', 'F', 2, 2, null, 10);
-CALL sp_insertUserAcquaintance(2, 'Angel', 'Angelov', 'M', 6, 1, null, 9);
 
 -- GENERATE MEETINGS 
-CALL sp_insertMeeting(1, 3, '2022-02-14 12:30', 2, null);
-CALL sp_insertMeeting(1, 4, '2022-01-23 20:45', 5, null);
-CALL sp_insertMeeting(1, 2, '2021-12-04 15:00', 1, null);
-CALL sp_insertMeeting(1, 5, '2021-09-27 09:30', 8, null);
-CALL sp_insertMeeting(1, 9, '2022-03-11 17:30', 7, null);
-CALL sp_insertMeeting(1, 2, '2022-02-14 12:00', 10, null);
-CALL sp_insertMeeting(1, 10, '2020-07-30 23:30', 4, null);
-CALL sp_insertMeeting(1, 4, '2021-05-28 10:30', 3, null);
-CALL sp_insertMeeting(1, 6, '2022-02-14 16:30', 8, null);
-CALL sp_insertMeeting(1, 6, '2022-02-14 12:50', 4, null);
+-- (user_id, acquaintance_id, datetime, reason_id, location_id, comments)
+CALL sp_insertUserMeeting(1, 1, '2022-02-14 12:30', 2, 4, null);
+CALL sp_insertUserMeeting(1, 4, '2022-01-23 20:45', 5, 2, null);
+CALL sp_insertUserMeeting(1, 2, '2021-12-04 15:00', 1, 7, null);
+CALL sp_insertUserMeeting(1, 5, '2021-09-27 09:30', 8, 8, null);
+CALL sp_insertUserMeeting(1, 9, '2022-03-11 17:30', 7, 1, null);
+CALL sp_insertUserMeeting(1, 2, '2022-02-14 12:00', 10, 9, null);
+CALL sp_insertUserMeeting(1, 10, '2020-07-30 23:30', 4, 10, null);
+CALL sp_insertUserMeeting(1, 4, '2021-05-28 10:30', 3, 2, null);
+CALL sp_insertUserMeeting(1, 6, '2022-02-14 16:30', 8, 4, null);
+CALL sp_insertUserMeeting(1, 6, '2022-02-14 12:50', 4, 1, null);
--- RM_Triggers.sql
+++ RM_Triggers.sql
@@ -27,14 +27,29 @@ DELIMITER ;
 
 
 DELIMITER $
-CREATE TRIGGER tg_del_checkAcquaintanceReferenceInUserAcquaintanceRelationships AFTER DELETE ON user_acquaintance_relationships FOR EACH ROW
+CREATE TRIGGER tg_del_deleteAcquaintanceFromParentTable AFTER DELETE ON user_acquaintance_relationships FOR EACH ROW
 BEGIN
-	IF((SELECT COUNT(*) FROM user_acquaintance_relationships WHERE acquaintance_id = OLD.acquaintance_id) = 0)
-    THEN DELETE FROM acquaintances WHERE id = OLD.acquaintance_id;
+	IF
+		((SELECT COUNT(*) FROM user_acquaintance_relationships WHERE acquaintance_id = OLD.acquaintance_id) = 0)
+	THEN
+		DELETE FROM acquaintances WHERE id = OLD.acquaintace_id;
 	END IF;
 END$
 DELIMITER ;
-drop trigger tg_del_checkAcquaintanceReferenceInUserAcquaintanceRelationships;
+
+
+DELIMITER $
+CREATE TRIGGER tg_del_abortDeleteAcquaintaces BEFORE DELETE ON acquaintances FOR EACH ROW
+BEGIN
+	IF
+		((SELECT COUNT(*) FROM user_acquaintance_relationships WHERE acquaintance_id = OLD.id) > 0)
+	THEN
+		SIGNAL SQLSTATE '45000'
+		SET MESSAGE_TEXT = 'Deletion aborted! There are child tables referencing this value.', MYSQL_ERRNO = 1001;
+	END IF;
+END$
+DELIMITER ;
+
 
 -- Cities -------------------------------------------------------------------------------
 /* CHECKED */
@@ -56,19 +71,72 @@ DELIMITER ;
 
 
 DELIMITER $
-CREATE TRIGGER tg_del_checkWhetherToDeleteRecordFromCities AFTER DELETE ON user_cities FOR EACH ROW
+CREATE TRIGGER tg_del_deleteCitiesFromParentTable AFTER DELETE ON user_cities FOR EACH ROW
 BEGIN
-	IF ((SELECT COUNT(*) FROM user_cities WHERE city_id = OLD.city_id) = 0)
-    THEN DELETE FROM cities WHERE id = OLD.city_id;
-    END IF;
+	IF
+		((SELECT COUNT(*) FROM user_cities WHERE city_id = OLD.city_id) = 0)
+	THEN
+		DELETE FROM cities WHERE id = OLD.city_id;
+	END IF;
 END$
 DELIMITER ;
 
 
-#DELIMITER $
-#CREATE TRIGGER tg_del_changeCityReferenceInAcquaintace BEFORE DELETE ON user_cities FOR EACH ROW
-#BEGIN
-#	IF
+DELIMITER $
+CREATE TRIGGER tg_del_abortDeleteCities BEFORE DELETE ON cities FOR EACH ROW
+BEGIN
+	IF
+		((SELECT COUNT(*) FROM user_cities WHERE city_id = OLD.id) > 0)
+	THEN
+		SIGNAL SQLSTATE '45000'
+		SET MESSAGE_TEXT = 'Deletion aborted! There are child tables referencing this value.', MYSQL_ERRNO = 1001;
+	END IF;
+END$
+DELIMITER ;
+
+
+-- Locations ----------------------------------------------------------------------------
+/* CHECKED */
+DELIMITER $
+CREATE TRIGGER tg_ins_lowerLocations BEFORE INSERT ON locations FOR EACH ROW
+BEGIN
+	SET NEW.location = LOWER(NEW.location);
+END$
+DELIMITER ;
+
+
+/* CHECKED */
+DELIMITER $
+CREATE TRIGGER tg_upd_lowerLocation BEFORE UPDATE ON locations FOR EACH ROW
+BEGIN
+	SET NEW.location = LOWER(NEW.location);
+END$
+DELIMITER ;
+
+
+DELIMITER $
+CREATE TRIGGER tg_del_deleteLocationFromParentTable AFTER DELETE ON user_locations FOR EACH ROW
+BEGIN
+	IF
+		((SELECT COUNT(*) FROM user_locations WHERE location_id = OLD.location_id) = 0)
+	THEN
+		DELETE FROM locations WHERE id = OLD.location_id;
+	END IF;
+END$
+DELIMITER ;
+
+
+DELIMITER $
+CREATE TRIGGER tg_del_abortDeleteLocations BEFORE DELETE ON locations FOR EACH ROW
+BEGIN
+	IF
+		((SELECT COUNT(*) FROM user_locations WHERE location_id = OLD.id) > 0)
+	THEN
+		SIGNAL SQLSTATE '45000'
+		SET MESSAGE_TEXT = 'Deletion aborted! There are child tables referencing this value.', MYSQL_ERRNO = 1001;
+	END IF;
+END$
+DELIMITER ;
 
 
 -- Occupations --------------------------------------------------------------------------
@@ -86,6 +154,31 @@ DELIMITER $
 CREATE TRIGGER tg_upd_lowerOccupations BEFORE UPDATE ON occupations FOR EACH ROW
 BEGIN
 	SET NEW.occupation = LOWER(NEW.occupation);
+END$
+DELIMITER ;
+
+
+DELIMITER $
+CREATE TRIGGER tg_del_deleteOccupationFromParentTable AFTER DELETE ON user_occupations FOR EACH ROW
+BEGIN
+	IF
+		((SELECT COUNT(*) FROM user_occupations WHERE occupation_id = OLD.occupation_id) = 0)
+	THEN
+		DELETE FROM occupations WHERE id = OLD.occupation_id;
+	END IF;
+END$
+DELIMITER ;
+
+
+DELIMITER $
+CREATE TRIGGER tg_del_abortDeleteOccupations BEFORE DELETE ON occupations FOR EACH ROW
+BEGIN
+	IF
+		((SELECT COUNT(*) FROM user_occupations WHERE occupation_id = OLD.id) > 0)
+	THEN
+		SIGNAL SQLSTATE '45000'
+		SET MESSAGE_TEXT = 'Deletion aborted! There are child tables referencing this value.', MYSQL_ERRNO = 1001;
+	END IF;
 END$
 DELIMITER ;
 
@@ -109,6 +202,31 @@ END$
 DELIMITER ;
 
 
+DELIMITER $
+CREATE TRIGGER tg_del_deleteReasonFromParentTable AFTER DELETE ON user_reasons FOR EACH ROW
+BEGIN
+	IF
+		((SELECT COUNT(*) FROM user_reasons WHERE reason_id = OLD.reason_id) = 0)
+	THEN
+		DELETE FROM reasons WHERE id = OLD.reason_id;
+	END IF;
+END$
+DELIMITER ;
+
+
+DELIMITER $
+CREATE TRIGGER tg_del_abortDeleteReasons BEFORE DELETE ON reasons FOR EACH ROW
+BEGIN
+	IF
+		((SELECT COUNT(*) FROM user_reasons WHERE reason_id = OLD.id) > 0)
+	THEN
+		SIGNAL SQLSTATE '45000'
+		SET MESSAGE_TEXT = 'Deletion aborted! There are child tables referencing this value.', MYSQL_ERRNO = 1001;
+	END IF;
+END$
+DELIMITER ;
+
+
 -- Relationships ------------------------------------------------------------------------
 /* CHECK */
 DELIMITER $
@@ -126,4 +244,30 @@ BEGIN
 	SET NEW.relationship = LOWER(NEW.relationship);
 END$
 DELIMITER ;
+
+
+DELIMITER $
+CREATE TRIGGER tg_del_deleteRelationshipFromParentTable AFTER DELETE ON user_relationships FOR EACH ROW
+BEGIN
+	IF
+		((SELECT COUNT(*) FROM user_relationships WHERE relationship_id = OLD.relationship_id) = 0)
+	THEN
+		DELETE FROM relationships WHERE id = OLD.relationship_id;
+	END IF;
+END$
+DELIMITER ;
+
+
+DELIMITER $
+CREATE TRIGGER tg_del_abortDeleteRelationship BEFORE DELETE ON relationships FOR EACH ROW
+BEGIN
+	IF
+		((SELECT COUNT(*) FROM user_relationships WHERE relationship_id = OLD.id) > 0)
+	THEN
+		SIGNAL SQLSTATE '45000'
+		SET MESSAGE_TEXT = 'Deletion aborted! There are child tables referencing this value.', MYSQL_ERRNO = 1001;
+	END IF;
+END$
+DELIMITER ;
+
 
