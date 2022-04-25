f9457514d52db8525b4e6875bc7ee141effa59e9
Nikola Marin <nightshpee20@gmail.com>
2022-04-25 15:22:15 +0300

    Uploading my progress (app works).

--- rmanager/editForm.Designer.cs
+++ rmanager/editForm.Designer.cs
@@ -30,18 +30,6 @@ namespace rmanager
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button8 = new System.Windows.Forms.Button();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.button9 = new System.Windows.Forms.Button();
-            this.button10 = new System.Windows.Forms.Button();
-            this.textBox4 = new System.Windows.Forms.TextBox();
             this.button11 = new System.Windows.Forms.Button();
             this.button12 = new System.Windows.Forms.Button();
             this.textBox5 = new System.Windows.Forms.TextBox();
@@ -53,117 +41,18 @@ namespace rmanager
             this.textBox7 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button17 = new System.Windows.Forms.Button();
+            this.button18 = new System.Windows.Forms.Button();
+            this.textBox8 = new System.Windows.Forms.TextBox();
             this.editFormDataGridRecordBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.editFormDataGridRecordBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(178, 102);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(106, 38);
-            this.button3.TabIndex = 8;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(290, 102);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(106, 38);
-            this.button4.TabIndex = 7;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(11, 102);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(161, 38);
-            this.textBox1.TabIndex = 6;
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(178, 146);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(106, 38);
-            this.button5.TabIndex = 11;
-            this.button5.Text = "button5";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(290, 146);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(106, 38);
-            this.button6.TabIndex = 10;
-            this.button6.Text = "button6";
-            this.button6.UseVisualStyleBackColor = true;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(11, 146);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(161, 38);
-            this.textBox2.TabIndex = 9;
-            // 
-            // button7
-            // 
-            this.button7.Location = new System.Drawing.Point(178, 190);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(106, 38);
-            this.button7.TabIndex = 14;
-            this.button7.Text = "button7";
-            this.button7.UseVisualStyleBackColor = true;
-            // 
-            // button8
-            // 
-            this.button8.Location = new System.Drawing.Point(290, 190);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(106, 38);
-            this.button8.TabIndex = 13;
-            this.button8.Text = "button8";
-            this.button8.UseVisualStyleBackColor = true;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(11, 190);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(161, 38);
-            this.textBox3.TabIndex = 12;
-            // 
-            // button9
-            // 
-            this.button9.Location = new System.Drawing.Point(178, 234);
-            this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(106, 38);
-            this.button9.TabIndex = 17;
-            this.button9.Text = "button9";
-            this.button9.UseVisualStyleBackColor = true;
-            // 
-            // button10
-            // 
-            this.button10.Location = new System.Drawing.Point(290, 234);
-            this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(106, 38);
-            this.button10.TabIndex = 16;
-            this.button10.Text = "button10";
-            this.button10.UseVisualStyleBackColor = true;
-            // 
-            // textBox4
-            // 
-            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox4.Location = new System.Drawing.Point(11, 234);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(161, 38);
-            this.textBox4.TabIndex = 15;
-            // 
             // button11
             // 
+            this.button11.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.button11.Location = new System.Drawing.Point(178, 278);
             this.button11.Name = "button11";
             this.button11.Size = new System.Drawing.Size(106, 38);
@@ -173,6 +62,7 @@ namespace rmanager
             // 
             // button12
             // 
+            this.button12.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.button12.Location = new System.Drawing.Point(290, 278);
             this.button12.Name = "button12";
             this.button12.Size = new System.Drawing.Size(106, 38);
@@ -182,6 +72,7 @@ namespace rmanager
             // 
             // textBox5
             // 
+            this.textBox5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox5.Location = new System.Drawing.Point(11, 278);
             this.textBox5.Name = "textBox5";
@@ -190,6 +81,7 @@ namespace rmanager
             // 
             // button13
             // 
+            this.button13.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.button13.Location = new System.Drawing.Point(178, 322);
             this.button13.Name = "button13";
             this.button13.Size = new System.Drawing.Size(106, 38);
@@ -199,6 +91,7 @@ namespace rmanager
             // 
             // button14
             // 
+            this.button14.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.button14.Location = new System.Drawing.Point(290, 322);
             this.button14.Name = "button14";
             this.button14.Size = new System.Drawing.Size(106, 38);
@@ -208,6 +101,7 @@ namespace rmanager
             // 
             // textBox6
             // 
+            this.textBox6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox6.Location = new System.Drawing.Point(11, 322);
             this.textBox6.Name = "textBox6";
@@ -216,6 +110,7 @@ namespace rmanager
             // 
             // button15
             // 
+            this.button15.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.button15.Location = new System.Drawing.Point(290, 366);
             this.button15.Name = "button15";
             this.button15.Size = new System.Drawing.Size(106, 38);
@@ -225,6 +120,7 @@ namespace rmanager
             // 
             // button16
             // 
+            this.button16.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.button16.Location = new System.Drawing.Point(178, 366);
             this.button16.Name = "button16";
             this.button16.Size = new System.Drawing.Size(106, 38);
@@ -234,6 +130,7 @@ namespace rmanager
             // 
             // textBox7
             // 
+            this.textBox7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox7.Location = new System.Drawing.Point(11, 366);
             this.textBox7.Name = "textBox7";
@@ -252,21 +149,54 @@ namespace rmanager
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(178, 58);
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(178, 49);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(106, 38);
+            this.button1.Size = new System.Drawing.Size(106, 39);
             this.button1.TabIndex = 32;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // panel1
             // 
-            this.button2.Location = new System.Drawing.Point(290, 58);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(106, 38);
-            this.button2.TabIndex = 31;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel1.Controls.Add(this.button17);
+            this.panel1.Controls.Add(this.button18);
+            this.panel1.Controls.Add(this.textBox8);
+            this.panel1.Location = new System.Drawing.Point(25, 462);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(353, 81);
+            this.panel1.TabIndex = 33;
+            // 
+            // button17
+            // 
+            this.button17.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button17.Location = new System.Drawing.Point(265, 25);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(106, 38);
+            this.button17.TabIndex = 36;
+            this.button17.Text = "button17";
+            this.button17.UseVisualStyleBackColor = true;
+            // 
+            // button18
+            // 
+            this.button18.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button18.Location = new System.Drawing.Point(153, 25);
+            this.button18.Name = "button18";
+            this.button18.Size = new System.Drawing.Size(106, 38);
+            this.button18.TabIndex = 35;
+            this.button18.Text = "button18";
+            this.button18.UseVisualStyleBackColor = true;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox8.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox8.Location = new System.Drawing.Point(-14, 25);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(161, 38);
+            this.textBox8.TabIndex = 34;
             // 
             // editFormDataGridRecordBindingSource
             // 
@@ -276,9 +206,9 @@ namespace rmanager
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(408, 416);
+            this.ClientSize = new System.Drawing.Size(409, 661);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button15);
             this.Controls.Add(this.button16);
@@ -289,22 +219,11 @@ namespace rmanager
             this.Controls.Add(this.button11);
             this.Controls.Add(this.button12);
             this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.button9);
-            this.Controls.Add(this.button10);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.button7);
-            this.Controls.Add(this.button8);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.textBox1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "editForm";
             this.Text = "Edit Form";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.editFormDataGridRecordBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -313,18 +232,6 @@ namespace rmanager
 
         #endregion
         private System.Windows.Forms.BindingSource editFormDataGridRecordBindingSource;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Button button8;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Button button9;
-        private System.Windows.Forms.Button button10;
-        private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Button button11;
         private System.Windows.Forms.Button button12;
         private System.Windows.Forms.TextBox textBox5;
@@ -336,6 +243,9 @@ namespace rmanager
         private System.Windows.Forms.TextBox textBox7;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Button button18;
+        private System.Windows.Forms.TextBox textBox8;
     }
 }
--- rmanager/editForm.cs
+++ rmanager/editForm.cs
@@ -34,10 +34,13 @@ namespace rmanager
                     column_name = "relationship";
                     break;
             }
-            addRow();
+            displayTable(name);
+            addRow(1, "hellooooo");
+            addRow(2, "thereeee");
+            addRow(3, "wwwwww");
         }
         
-        private void setDataGridValues(string name)
+        private void displayTable(string name)
         {
             MySqlDataAdapter da = new MySqlDataAdapter();
             switch(name)
@@ -74,41 +77,77 @@ namespace rmanager
             da.Fill(ds, name);
             dt = ds.Tables[name];
 
-
+            //for (int i = 0; i < dt.Rows.Count; i++)
+            //{
+            //    MessageBox.Show(dt.Rows[i][0].ToString());
+            //    //addRow(i + 1, dt.Rows[i][0].ToString());
+            //}
         }
 
-        private TextBox addTextBox()
+        private TextBox addTextBox(int id, string txtValue)
         {
             TextBox txt = new TextBox();
             //txt.Top = 47;
             //txt.Left = 8;
             //txt.Text = textBoxValue;
+            txt.Name = "txt" + id;
+            txt.Text = txtValue;
             txt.Font = new Font(txt.Font.FontFamily, 12);
-            txt.Width = 122;
+            txt.Width = txtValue.Length * 12;
+            txt.Top = 3;
+            txt.ReadOnly = true;
+
+            //Experiment
+            //txt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            //txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            //txt.Location = new System.Drawing.Point(11, 278);
+            //txt.Name = "txt" + id;
+            //txt.Size = new System.Drawing.Size(161, 38);
+            //txt.TabIndex = 18;
 
             return txt;
         }
-        private Button addButton()
+        private Button addButton(int id)
         {
             Button btn = new Button();
+            btn.Name = "btn" + id;
             //btn.Top = 47;
-            //btn.Left = 134;
-            btn.Height = 31;
+            btn.Left = 126;
+            btn.Height = 32;
             btn.Width = 80;
             return btn;
         }
         //TextBox txt, Button edt, Button del
-        private void addRow()
+        private void addRow(int id, string txtValue)
         {
             Panel pnl = new Panel();
             this.Controls.Add(pnl);
 
-            pnl.Top = 47;
+            pnl.Name = "row" + id;
+            pnl.Top = 40*id;
             pnl.Left = 8;
-            pnl.Height = 31;
+            pnl.Height = 32;
             pnl.Width = 290;
+            pnl.Anchor = (System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right));
             pnl.BackColor = Color.DarkGray;
+
+            Button btn = addButton(id);
+            pnl.Controls.Add(btn);
+            btn = addButton(id);
+            btn.Left += 82;
+            pnl.Controls.Add(btn);
             
+            pnl.Click += new System.EventHandler(this.pnl_Click);
+
+            TextBox txt = addTextBox(id, txtValue);
+            pnl.Controls.Add(txt);
         }
+
+        private void pnl_Click(object Sender, EventArgs e)
+        {
+            Panel pnl = Sender as Panel;
+            MessageBox.Show(pnl.Name);
+        }
+       
     }
 }
