e12e3f074765aa109e6c68731c9f3b758943334d
Nikola Marin <nightshpee20@gmail.com>
2022-05-12 23:42:11 +0300

    Solution kinda works but could be better.

--- rmanager/userProfileForm.cs
+++ rmanager/userProfileForm.cs
@@ -17,6 +17,7 @@ namespace rmanager
         private int user_id;
         private mainForm parent;
         public List<string> user_info = new List<string>();
+        private bool visibleScroll = false;
        
 
         public userProfileForm(mainForm parent, int user_id)
@@ -70,7 +71,7 @@ namespace rmanager
 
             dgv.DataSource = dt;
 
-            if(dt.Rows.Count > 20 && meetingsDataGridView.Controls.OfType<VScrollBar>().First().Visible != true)
+            if (meetingsDataGridView.Controls.OfType<VScrollBar>().First().Visible == true && visibleScroll == false)
             {
                 meetingsDataGridView.Width += 17;
                 addMeetingButton.Left += 9;
@@ -78,7 +79,9 @@ namespace rmanager
                 userStatsButton.Left += 9;
                 helpButton.Left += 9;
                 switchUserButton.Left += 9;
+                visibleScroll = true;
             }
+            else visibleScroll = false;
         }
         
         private void returnButton_Click(object sender, EventArgs e)
